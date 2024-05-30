35cddcc1d1d7884b6a9a95f1e1a4711a154db9e4
ANZAR <anzarkhan790@gmail.com>
2024-05-30 15:04:00 +0530

    new comm

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,5 +1,7 @@
 import { motion,useScroll, useTransform } from "framer-motion"
 import { useRef } from "react"
+import Mountains from '../../../dist/mountains.png';
+import Stars from '../../../dist/stars.png'
 import "./parallax.scss"
 const Parallax = ({type}) => {
   const ref=useRef()
@@ -17,9 +19,13 @@ const Parallax = ({type}) => {
         :"linear-gradient(180deg,#0c0c1d,#505064)"
     }}>
         <motion.h1 style={{y:yText}}>{type==="about"?"About Me":"My Projects"}</motion.h1>
-        <motion.div className="mountains"></motion.div>
+        <motion.div className="mountains">
+          <img src={Mountains} alt="" />
+        </motion.div>
         <motion.div style={{y:yBG,backgroundImage:`url(${type==="about"?"./planets.png":"./sun.png"})`}} className="planets"></motion.div>
-        <motion.div className="stars"></motion.div>     
+        <motion.div className="stars">
+          <img src={Stars} alt="" />  
+        </motion.div>     
         
     </div>
   )
